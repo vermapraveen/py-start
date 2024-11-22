dfb60b94b972a9525d27df04477b8894486ec90a
praveen <pkv@divyamachine.com>
2024-11-22 05:46:06 -0600

    derivatives using sympy

---
+++ ML_Projects/notebooks/partial_derivative.py
@@ -0,0 +1,19 @@
+import sympy as sp
+
+x_val, y_val = 3, 4
+x_1, y_1 = sp.symbols("x y")
+
+# define a function
+f_1 = x_1**2 + y_1**2
+
+#################### sympy #####################
+# compute 'symbolic' gradient
+df_dx_sympy = sp.diff(f_1, x_1)
+df_dy_sympy = sp.diff(f_1, y_1)
+
+# Convert symbolic gradient to numeric functions
+df_dx_func_sympy = sp.lambdify((x_1, y_1), df_dx_sympy)
+df_dy_func_sympy = sp.lambdify((x_1, y_1), df_dy_sympy)
+
+print("sympy df/dx:", df_dx_func_sympy(x_val, y_val))
+print("sympy df/dy:", df_dy_func_sympy(x_val, y_val))
